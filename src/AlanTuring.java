aade535670ab85aad176e2633f51988beedb1517
Jesse <nguyen.jesse0@gmail.com>
2022-08-28 16:46:26 -0700

    CIS 12 Java-Assignment-001

--- src/AlanTuring.java
+++ src/AlanTuring.java
@@ -1,9 +1,18 @@
 public class AlanTuring {
+    /*
+
+      @author Trevor Hartman
+     * @author Jesse Nguyen
+     *
+     * @since Version 1.0
+
+     */
     /**
      * Prints Alan Turing to the console
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
         // Write your program here
+        System.out.printf("%s%n","Alan Turing");
     }
 }
--- src/JurassicPark.java
+++ src/JurassicPark.java
@@ -1,9 +1,18 @@
 public class JurassicPark {
+    /*
+
+      @author Trevor Hartman
+     * @author Jesse Nguyen
+     *
+     * @since Version 1.0
+
+     */
     /**
-     * Prints famous Jurasic Park quote to console with only 1 sout statement.
+     * Prints famous Jurassic Park quote to console with only 1 sout statement.
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
         // Write your program here
+        System.out.println("\"Life, uh,\nfinds a way.\"\n-Ian Malcom");
     }
 }
